abbc788716c8085f40ee1e05f1fdc2124385f424
Sonu Singh <sonusingh@Sonus-MacBook-Air.local>
2026-07-28 23:22:21 +0530

    New Version

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+
+async function loadPage(page){
+    const response = await fetch("pages/" + page);
+    const html = await response.text();
+    const htmlPage = document.getElementById("content")
+    htmlPage.innerHTML = html;
+}
+
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button, index) => {
-    button.addEventListener('click', () => {
+    button.addEventListener('click', (event) => {
         const buttonTarget = button.dataset.bsTarget; 
 
         const subtopic = document.querySelector(buttonTarget);
@@ -14,3 +22,16 @@ buttons.forEach((button, index) => {
         subtopic.classList.toggle("open");
     });
 });
+
+const navlinks = document.querySelectorAll(".nav-link")
+
+navlinks.forEach((navlink) => {
+    navlink.addEventListener('click', (event) => {
+
+        event.preventDefault();
+        const pageName = navlink.dataset.page;
+
+        loadPage(pageName);
+    });
+})
+loadPage("introduction/overview.html");
